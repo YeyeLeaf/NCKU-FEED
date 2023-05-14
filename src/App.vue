83067d9c9a0184c104d078574cb0b338067e6ce1
salmoniscute <noralin249@gmail.com>
2023-05-14 14:57:46 +0800

    footer下面那行小字還沒搞定

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Footer from './components/Footerr.vue'
+import Navbar1 from './components/Navbar1.vue'
+import Navbar2 from './components/Navbar2.vue'
+
 </script>
 
 <template>
-  <HelloWorld />
+  <Footer />
+  <Navbar1 />
+  <!-- <Navbar2 /> -->
+  
 </template>
 
 <style scoped>
---
+++ src/components/Navbar1.vue
@@ -0,0 +1,53 @@
+// 已登入
+<template>
+  <nav class="flex items-center justify-between flex-wrap bg-[#FF8E3C] p-4 lg:p-0 sticky ">
+    <div class="lg:flex items-center text-white mr-6 ml-4">
+      <a href="#" class="hidden lg:block">
+            <picture>
+              <img src="../assets/nckufeed_white.svg" class="lg:h-20 lg:mx-2 lg:pl-8">
+            </picture>
+      </a>
+      <span class="font-montserrat font-bold text-2xl tracking-tight">NCKU FEED</span>
+    </div>
+    <div class="block lg:hidden mr-4">
+      <button @click="isOpen = !isOpen" class="flex items-center px-3 py-2 border rounded text-white border-white hover:text-gray-300 hover:border-gray-300">
+        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0zm0 6h20v2H0zm0 6h20v2H0z"/></svg>
+      </button>
+    </div>
+
+    <!-- lg留著如果之後有需要讓他們排列在navbar就可以用 -->
+    <div :class="{'hidden': !isOpen}" class="w-full block flex-grow lg:flex lg:items-center lg:w-auto lg:hidden">
+      <div class="text-base lg:flex-grow ">
+        <a href="#" class="block mt-4 ml-4 lg:inline-block lg:mt-0 text-white hover:text-gray-300 mr-4">
+          個人頁面
+        </a>
+        <a href="#" class="block mt-4 ml-4 lg:inline-block lg:mt-0 text-white hover:text-gray-300 mr-4">
+          收藏清單
+        </a>
+      </div>
+    </div>
+
+    <div class="hidden lg:block lg:flex mx-12">
+            <a href="#">
+                <picture>
+                   <img src="../assets/bookmark.png" class="h-10 lg:h-12 mx-3 lg:mx-6">
+                </picture>
+            </a>
+            <a href="#">
+                <picture>
+                   <img src="../assets/user.png" class="h-10 lg:h-12 mx-3 lg:mx-6">
+                </picture>
+            </a>
+        </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false
+    }
+  }
+}
+</script>
