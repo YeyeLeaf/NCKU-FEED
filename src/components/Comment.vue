ac0a7cf2fc39e8b7360ca5ee37c4978153d40ed3
salmoniscute <noralin249@gmail.com>
2023-09-01 16:51:22 +0800

    add comment function

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,17 +1,70 @@
 <script setup>
-import Star from './Star.vue'
-defineProps({
-  img: String,
-  content: String,
-  stars: Array  
+import Star from './Star.vue';
+import { storage} from '../firebase/firebaseConfig';
+import {  ref as firebaseRef,getDownloadURL } from 'firebase/storage';
+import { ref as vueRef } from 'vue';
+import { user } from '../class.js';
+import { textTruncation } from '../eventBus';
+
+const props = defineProps({
+    infor: Object
 })
+
+const emit = defineEmits(['delete-comment','edit-comment']);
+
+const img = vueRef('');
+//取得使用者大頭貼照片
+const getUserPic = async () =>{
+  if (!props.infor){
+    return
+  }
+  try {
+    img.value = await getDownloadURL(firebaseRef(storage, 'userIcon/' + props.infor.uid));
+  } catch (error) {
+    console.error(error);
+  }
+}
+getUserPic();
+
+const isMe = vueRef(false);
+if (props.infor.uid == user.id){
+  isMe.value = true;
+}
+const deleteComment = async () => { 
+  await fetch("http://localhost:5000/comments", {
+    method: "DELETE",
+    headers: {
+    "Authorization": `Bearer ${user.access_token}`,
+    'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        'id':props.infor._id
+    })
+  })
+  .then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  emit('delete-comment')
+};
+
 </script>
 <template>
   <div class="flex w-full mb-2 justify-between items-center shadow-md shadow-slate-300 rounded-md p-4 border-2 border-slate-100">
     <div class="flex">
         <img :src="img" class="h-8 rounded-full mr-3">
-        <p class="text-md text-left">{{ content }}</p>
+        <p class="text-md text-left">{{ textTruncation(infor.content,25) }}</p>
     </div>
-    <Star :stars="stars"/>
+    <div class="flex space-x-5">
+      <div v-if="isMe" class="flex space-x-3 mt-2">
+        <i class="fa fa-pen" @click="$emit('edit-comment')"></i>
+        <i class="fa fa-trash" @click="deleteComment"></i>
+      </div>
+      <Star :stars="infor.rating"/>
+    </div>
   </div>
 </template>
--- src/components/Star.vue
+++ src/components/Star.vue
@@ -1,25 +1,41 @@
 <script setup>
 import { ref } from 'vue';
-defineProps({
-  stars: Array
+const props = defineProps({
+    stars: Object
 })
 
-
-
-const rating = ["環境整潔", "服務態度", "美味程度", "CP值", "綜合評分"];
+const rating = ["環境整潔", "服務態度", "美味程度", "CP值"];
 const isShow = ref(false);
 const btnToggle = () => {
   isShow.value = !isShow.value;
 }
+const rated_number = ref([0,0,0,0]);
+let ave = 0.0;
+const objectToArray = () =>{
+  if (!props.stars){
+    return;
+  }
+  rated_number.value[0] = props.stars.cleanliness;
+  rated_number.value[1] = props.stars.service;
+  rated_number.value[2] = props.stars.deliciousness;
+  rated_number.value[3] = props.stars.CPR;
+  let sum = 0.0;
+  for (let i = 0 ; i<4;i++){
+    sum+=rated_number.value[i];
+  }
+  ave = sum/4.0;
+}
+
+objectToArray();
 </script>
 <template>
-  <button class="bg-[#ff8e3c] text-white rounded-2xl py-1 px-2 w-28 relative">{{ stars[4] }}&nbsp;<i class="fas fa-star" @click="btnToggle"></i>
+  <button class="bg-[#ff8e3c] text-white rounded-2xl py-1 px-2 w-24 relative">{{ ave }}&nbsp;<i class="fas fa-star" @click="btnToggle"></i>
     <div v-if="isShow" class="absolute detail bg-white border-2 border-[#ff8e3c] p-4 w-56 right-0 top-full z-10">
       <p v-for="(item, index) in rating" :key="index" class="text-black flex justify-between items-center">
         <span class="w-6/12">{{ item }}</span>
         <span>
-          <i v-for="n in stars[index]" class="fas fa-star text-[#ff8e3c]"></i>
-          <i v-for="n in (5 - stars[index])" class="far fa-star text-[#ff8e3c]"></i>
+          <i v-for="n in rated_number[index]" class="fas fa-star text-[#ff8e3c]"></i>
+          <i v-for="n in (5 - rated_number[index])" class="far fa-star text-[#ff8e3c]"></i>
         </span>
       </p>
     </div>
